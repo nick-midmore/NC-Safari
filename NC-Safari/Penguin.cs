66557d124fd0d394e1ac96d9a2127d614722b844
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-13 10:18:31 +0000

    Added some concrete animal classes

---
+++ NC-Safari/Penguin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NC_Safari
+{
+    internal class Penguin : Bird, IMove
+    {
+        public Penguin(float Weight) : base(Weight)
+        {
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Penguin is moving!");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("glip glop glip glop glip glop SKREEEEEEEEEEEEEE");
+        }
+    }
+}
---
+++ NC-Safari/Tiger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NC_Safari
+{
+    internal class Tiger : PantheraBigCat, IMove
+    {
+        public Tiger(float Weight) : base(Weight)
+        {
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Tiger is moving!");
+        }
+    }
+}
